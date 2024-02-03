725e22e65791d9e3ae02e9f37a81cb3d6631b416
sidsidu <47473176+sidsidu@users.noreply.github.com>
2024-02-03 21:50:50 +0530

    Add files via upload

---
+++ yo.js
@@ -0,0 +1,10 @@
+function yes()
+{
+    var img = document.getElementById('image');
+    img.src = 'yess.gif';
+}
+function no()
+{
+    var img = document.getElementById('image');
+    img.src = 'no.gif';
+}
