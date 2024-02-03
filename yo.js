fcb4856b5dbcfd0dbd57385b4e4d5a3ea780c249
sidsidu <47473176+sidsidu@users.noreply.github.com>
2024-02-03 22:07:09 +0530

    Update yo.js

--- yo.js
+++ yo.js
@@ -2,9 +2,11 @@ function yes()
 {
     var img = document.getElementById('image');
     img.src = 'yess.gif';
+    var text1 = document.getElementById('start').innerText='YAAAAYYYYYYYYYYYYYYYYYYYYYY'
 }
 function no()
 {
     var img = document.getElementById('image');
     img.src = 'no.gif';
+    var text1 = document.getElementById('start').innerText='NOOOOOOOOOOOOOOOOOOOOOOOOOOOO :('
 }
